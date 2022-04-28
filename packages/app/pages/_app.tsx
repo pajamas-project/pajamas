5826a795c3c424398f7e76720bfc353d9765b70d
Mikhail Danshin <effectdopler0@gmail.com>
2022-04-28 12:36:23 +0300

    feat(app): add integration with nhost

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createCache from '@emotion/cache'
+import { NhostClient, NhostNextProvider } from '@nhost/nextjs'
+import { NhostApolloProvider } from '@nhost/react-apollo'
 import type { AppProps } from 'next/app'
+import { FC } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const nhost = new NhostClient({
+  backendUrl: 'https://bxcmyzroksvtjxbjhkms.nhost.run',
+})
+
+function createEmotionCache() {
+  return createCache({ key: 'css' })
 }
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const MyApp: FC<MyAppProps> = ({ Component, pageProps, emotionCache = clientSideEmotionCache }) => (
+  <CacheProvider value={emotionCache}>
+    {/* @ts-ignore */}
+    <NhostNextProvider nhost={nhost}>
+      {/* @ts-ignore */}
+      <NhostApolloProvider nhost={nhost}>
+        <Component {...pageProps} />
+      </NhostApolloProvider>
+    </NhostNextProvider>
+  </CacheProvider>
+)
 
 export default MyApp
