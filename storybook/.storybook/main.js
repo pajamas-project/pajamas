27608cc1c7a7dc3e3bd8d8c6f9519b8f64ce809c
Mikhail Danshin <effectdopler0@gmail.com>
2022-05-09 13:56:31 +0300

    fix(storybook): run storybook

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,12 +1,5 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
+  stories: ['../../packages/**/*.stories.mdx', '../../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
 }
