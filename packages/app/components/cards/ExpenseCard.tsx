388f4e651aca039a8055fbe2032bbda45cb42c6d
Mikhail Danshin <effectdopler0@gmail.com>
2022-05-10 14:21:58 +0300

    fix(app): remove unused components

--- packages/app/components/cards/ExpenseCard.tsx
+++ packages/app/components/cards/ExpenseCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { Card } from '@pajamas/core-components'
 import { FC } from 'react'
