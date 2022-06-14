435a6e25c62d3a2f7b02a2f261410664db379540
Mikhail Danshin <effectdopler0@gmail.com>
2022-06-14 14:26:24 +0300

    feat(app): temp add button for navigate to portfolio page

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,12 +1,14 @@
-import { Grid } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import { MainLayout } from '@pajamas/core-components'
 import { NextPage } from 'next'
+import Link from 'next/link'
 import React from 'react'
 import { DailyBudgetCard, ExpenseCard, ExpenseTable, IncomeCard, OperationPieChar } from '../components'
+import { withAuth } from '../hoc/withAuth'
 
 const Home: NextPage = () => (
   <MainLayout>
-    <Grid container spacing={3}>
+    <Grid container spacing={3} alignSelf={'start'}>
       <Grid item xs={12}>
         <OperationPieChar />
       </Grid>
@@ -26,8 +28,14 @@ const Home: NextPage = () => (
       <Grid item xs={12}>
         <ExpenseTable />
       </Grid>
+
+      <Grid item lg={4} md={4} sm={4} xs={12}>
+        <Link href={'/portfolio'} passHref>
+          <Button variant={'contained'}>Portfolio</Button>
+        </Link>
+      </Grid>
     </Grid>
   </MainLayout>
 )
 
-export default Home
+export default withAuth(Home)
