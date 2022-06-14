eee9bb46d44c5afe71ccda66fc2bfef79ed09af4
Mikhail Danshin <effectdopler0@gmail.com>
2022-06-14 14:28:05 +0300

    feat(app): add auth protected hoc

---
+++ packages/app/hoc/withAuth.tsx
@@ -0,0 +1,26 @@
+import { Box, CircularProgress } from '@mui/material'
+import { useAuthenticationStatus } from '@nhost/nextjs'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export const withAuth = (Component: React.FC) =>
+  // eslint-disable-next-line func-names
+  function AuthProtected<P>(props: P) {
+    const router = useRouter()
+    const { isLoading, isAuthenticated } = useAuthenticationStatus()
+
+    if (isLoading && !isAuthenticated) {
+      return (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      )
+    }
+
+    if (!isAuthenticated) {
+      router.push('/signin')
+      return null
+    }
+
+    return <Component {...props} />
+  }
