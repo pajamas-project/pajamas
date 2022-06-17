2cd9c3460a0cd5d90d4d1020ae660acc0cf7f436
Mikhail Danshin <effectdopler0@gmail.com>
2022-06-17 16:44:01 +0300

    feat(app): add icons on signin page

--- packages/app/pages/signin.tsx
+++ packages/app/pages/signin.tsx
@@ -1,10 +1,12 @@
-import { Grid, Card, Stack } from '@mui/material'
+import { Grid, Paper, Stack } from '@mui/material'
 import { useProviderLink } from '@nhost/react'
 import { MainLayout } from '@pajamas/core-components'
+import Image from 'next/image'
 import Link from 'next/link'
 import { GithubLoginButton, GoogleLoginButton } from 'react-social-login-buttons'
 import { NextPage } from 'next'
 import React from 'react'
+import pajamas from './../public/pajamas.png'
 
 const SignIn: NextPage = () => {
   const { github, google } = useProviderLink()
@@ -12,15 +14,24 @@ const SignIn: NextPage = () => {
   return (
     <MainLayout>
       <Grid container alignItems={'center'} justifyContent={'center'}>
-        <Grid item container xs={12} sm={6} alignItems={'center'} justifyContent={'center'}>
-          <Stack component={Card} spacing={2} p={1} width={400}>
-            <Link href={github} passHref>
-              <GithubLoginButton />
+        <Grid item container xs={12} sm={6} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+          <Grid mt={-20} item xs={12}>
+            <Link href={'/'} passHref>
+              <Image src={pajamas} alt={'Pajamas'} style={{ cursor: 'pointer' }} />
             </Link>
-            <Link href={google} passHref>
-              <GoogleLoginButton />
-            </Link>
-          </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper>
+              <Stack spacing={2} p={1}>
+                <Link href={github} passHref>
+                  <GithubLoginButton />
+                </Link>
+                <Link href={google} passHref>
+                  <GoogleLoginButton />
+                </Link>
+              </Stack>
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
     </MainLayout>
