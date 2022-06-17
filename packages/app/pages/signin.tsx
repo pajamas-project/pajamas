c2d6c82a736bc3a83a213773b78d83ef939a5fd7
Mikhail Danshin <effectdopler0@gmail.com>
2022-06-17 12:35:36 +0300

    feat(app): google auth

--- packages/app/pages/signin.tsx
+++ packages/app/pages/signin.tsx
@@ -1,22 +1,26 @@
-import { Grid, Card } from '@mui/material'
+import { Grid, Card, Stack } from '@mui/material'
+import { useProviderLink } from '@nhost/react'
 import { MainLayout } from '@pajamas/core-components'
-import { useRouter } from 'next/router'
-import { GithubLoginButton } from 'react-social-login-buttons'
+import Link from 'next/link'
+import { GithubLoginButton, GoogleLoginButton } from 'react-social-login-buttons'
 import { NextPage } from 'next'
 import React from 'react'
 
 const SignIn: NextPage = () => {
-  const router = useRouter()
+  const { github, google } = useProviderLink()
 
   return (
     <MainLayout>
       <Grid container alignItems={'center'} justifyContent={'center'}>
-        <Grid item xs={12} sm={6}>
-          <Card sx={{ padding: 2 }}>
-            <GithubLoginButton
-              onClick={() => router.push('https://bxcmyzroksvtjxbjhkms.nhost.run/v1/auth/signin/provider/github')}
-            />
-          </Card>
+        <Grid item container xs={12} sm={6} alignItems={'center'} justifyContent={'center'}>
+          <Stack component={Card} spacing={2} p={1} width={400}>
+            <Link href={github} passHref>
+              <GithubLoginButton />
+            </Link>
+            <Link href={google} passHref>
+              <GoogleLoginButton />
+            </Link>
+          </Stack>
         </Grid>
       </Grid>
     </MainLayout>
